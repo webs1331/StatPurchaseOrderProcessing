d144103bbeb8e55dc78a9f71fd223ec4ff873b72
Trent Powell <opethian9@outlook.com>
2022-05-15 11:42:20 -0500

    removed old/unused method

--- StatPurchaseOrderProcessing/Excel/ExcelUtility.cs
+++ StatPurchaseOrderProcessing/Excel/ExcelUtility.cs
@@ -2,23 +2,10 @@
 {
     using CsvHelper;
     using CsvHelper.Configuration;
-    using StatPurchaseOrderProcessing.S3;
     using System.Globalization;
 
     public static class ExcelUtility
     {
-        public static List<Claim> GetClaimsData(string filePath)
-        {
-            var config = new CsvConfiguration(CultureInfo.CurrentCulture) { Delimiter = "~" };
-
-            using (var reader = new StreamReader(filePath))
-            using (var csv = new CsvReader(reader, config))
-            {
-                csv.Context.RegisterClassMap<ClaimMap>();
-                return csv.GetRecords<Claim>().ToList();
-            }
-        }
-
         public static List<T> ReadMappedData<T, TMap>(string filePath, string delimeter) where TMap : ClassMap<T>
         {
             var config = new CsvConfiguration(CultureInfo.CurrentCulture) { Delimiter = delimeter };
